de24444f994d1530b5c8f1f248f5cc553d9be14d
Aaron Dweck <aarondweck24@gmail.com>
2025-02-28 10:31:15 +0000

    added page routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+import { Routes, Route } from 'react-router'
 import './App.css'
+import LandingPage from './components/LandingPage/LandingPage'
+import Calendar from './components/Calendar/Calendar'
+import Register from './components/Register/Register'
+import Login from './components/Login/Login'
 
 function App() {
+    return(
+        <Routes>
+            <Route path='/' element={<LandingPage />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/calendar' element={<Calendar />} />
+        </Routes>
+    )
   
 }
 
